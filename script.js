d23613915c1cd931da91fb9ffa3b998734590224
Racer <reaganjoseph26@yahoo.com>
2020-11-01 20:18:37 -0500

    upperCase criteria set

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 // Assignment code here
 let generatePassword = function () {
   // Variables associated with specific criteria
-  //site for all listed criteria https://net-comber.com/charset.html
-  let upperCase = String.fromCharCode(Math.floor(Math.random() * 26) + 65); // Random uppercase letter
-  let lowerCase = String.fromCharCode(Math.floor(Math.random() * 26) + 97); // Random lowercase letter
-  let number = String.fromCharCode(Math.floor(Math.random() * 10) + 48); // random number
-  let symbol = String.fromCharCode(Math.floor(Math.random() * 15) + 33); // random special character
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWUXYZ" // Random uppercase letter
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz" // Random lowercase letter
+  let number = "0123456789" // random number
+  let symbol = "!#$%&*+-.:;<=>?@^_{|}~" // random special character
+  let passArr = [];
+  var myArr = "";
   window.alert("Select criteria for password");//OPENING ALERT
   //Criteria for Password
   // create an if statement that conditions the user to make a valid input. If not valid alert will pop up.
-  var characterLenghtPrompt = window.prompt("Enter the length of characters you wish to have. Character length must be ATLEAST 8 and NO MORE than 128.");
-  if (characterLenghtPrompt === "" || characterLenghtPrompt === null || characterLenghtPrompt < 8 || characterLenghtPrompt > 128) {
+  var characterLengthPrompt = parseInt(window.prompt("Enter the length of characters you wish to have. Character length must be ATLEAST 8 and NO MORE than 128."));
+  if (characterLengthPrompt === "" || characterLengthPrompt === null || characterLengthPrompt < 8 || characterLengthPrompt > 128) {
     window.alert("Please eneter a valid character length.");
-    return generatePassword();
-    // if characterLenght is not true then continue through function
-  } 
-  // create a for loop that spits out the character that user enters
+    return ""
+  }
+  // if user confirms upperCase letters then they will be stored in passArr.
+  // if user declines upperCase letters then an empty string will be returned
+  var confirmUpper = window.confirm("Would you like to have UPPERCASE characters in your password?");
+  if (confirmUpper) {
+    window.alert("UPPERCASE letters have been selected in your password."); 
+    passArr += upperCase;
 
-
-  window.confirm("Would you like to have UPPERCASE characters in your password?");
-  window.confirm("Would you like to have lowercase characters in your password?");
-  window.confirm("Would you like to have numbers in your password?");
-  window.confirm("Would you like to have special characters?");
-
+    //console.log(passArr.push(upperCase[0]))
+  }
+  else {
+    window.alert("NO UPPERCASE CHARACTERS WILL BE IN YOUR PASSWORD.");
+    confirmUpper = ""
+  }
 };
-generatePassword()
-console.log(String.fromCharCode(Math.floor(Math.random() * 15) + 33));
+
+
+  return generatePassword()
+
+
 
 
 
